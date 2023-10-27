963c6d5e8ca1cdf9e5f62856b6bf6f430f50888e
phjppo0918 <phjppo0918@gmail.com>
2023-10-27 16:29:59 +0900

    feat : AnswerRes 계층화

--- src/main/kotlin/site/qbox/qboxserver/domain/answer/query/dto/AnswerRes.kt
+++ src/main/kotlin/site/qbox/qboxserver/domain/answer/query/dto/AnswerRes.kt
@@ -1,11 +1,13 @@
 package site.qbox.qboxserver.domain.answer.query.dto
 
-import com.querydsl.core.annotations.QueryProjection
 import site.qbox.qboxserver.domain.member.query.dto.MemberRes
 
-data class AnswerRes @QueryProjection constructor(
+data class AnswerRes(
     val content: String,
     val questionId: Long,
     val writer: MemberRes,
-    val comments: List<AnswerCommentRes>?
-)
+    val comments: List<AnswerCommentRes>
+) {
+    constructor(res: AnswerSummary, comments: List<AnswerCommentRes>) :
+            this(res.content, res.questionId, res.writer, comments)
+}
