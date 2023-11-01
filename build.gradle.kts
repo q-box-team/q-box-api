c193eebaf73a69537bc05cc602a62980aa81d588
phjppo0918 <phjppo0918@gmail.com>
2023-11-01 23:34:51 +0900

    feat: redis 라이브러리 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("com.github.maricn:logback-slack-appender:1.6.1")
-    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     //kotlin
