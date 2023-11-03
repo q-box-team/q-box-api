8bfd9f4b2b6df18cf74f7b4187cedcbb0c19f3df
phjppo0918 <phjppo0918@gmail.com>
2023-11-03 16:36:49 +0900

    refactor : 기존 memberRes -> MemberSummary로 이름 변경

--- src/main/kotlin/site/qbox/qboxserver/domain/answer/query/dto/AnswerCommentRes.kt
+++ src/main/kotlin/site/qbox/qboxserver/domain/answer/query/dto/AnswerCommentRes.kt
@@ -1,10 +1,10 @@
 package site.qbox.qboxserver.domain.answer.query.dto
 
 import com.querydsl.core.annotations.QueryProjection
-import site.qbox.qboxserver.domain.member.query.dto.MemberRes
+import site.qbox.qboxserver.domain.member.query.dto.MemberSummary
 
 data class AnswerCommentRes @QueryProjection constructor(
     val id: Long,
     val content: String,
-    val writer: MemberRes,
+    val writer: MemberSummary,
 )
